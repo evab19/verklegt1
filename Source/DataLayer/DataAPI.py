ce115b082f1a7a78253595a10601056abe786fa8
gunnsteinng <gunnsteinng07@ru.is>
2019-12-06 17:56:56 +0000

    airplanes

--- Source/DataLayer/DataAPI.py
+++ Source/DataLayer/DataAPI.py
@@ -1,5 +1,8 @@
 from models.Destination import Destination
+<<<<<<< Updated upstream
 import csv
+=======
+>>>>>>> Stashed changes
 
 class DataAPI:
     
@@ -7,6 +10,7 @@ class DataAPI:
         self.__destinations = []
 
     def add_destination(self, destination):
+<<<<<<< Updated upstream
         country = destination.get_country()
         airport = destination.get_airport()
         duration = destination.get_duration()
@@ -31,3 +35,31 @@ class DataAPI:
     
     def update_destination(self, airport_name):
         pass
+=======
+        # first add to file then to private list
+        with open("./data/destinations.txt", "a+") as destinations_file:
+            country = destination.get_country()
+            airport = destination.get_airport()
+            duration = destination.get_duration()
+            distance = destination.get_distance()
+            contact_name = destination.get_contact_name()
+            contact_phone = destination.get_contact_phone()
+            destinations_file.write("{}, {}, {}, {}, {}, {}\n".format(country, airport, duration, distance, contact_name, contact_phone))
+        destinations_file.close()
+
+    def get_destinations(self):
+        if self.__destinations == []:
+            destinations_str = ""
+            # destinations_str = "{}\t\t {}\t\t {}\t\t {}\t\t {}\t\t {}\n".format("Country", "Airport", "Duration", "Distance", "Contact name", "Contact phone")
+            with open("./data/destinations.txt", "r") as destination_file:
+                for line in destination_file.readlines():
+                    country, airport, duration, distance, contact_name, contact_phone = line.split(",")
+                    # title, genre, length = line.split(",")
+                    destinations_str += "{}, {}, {}, {}, {}, {}".format(country, airport, duration, distance, contact_name, contact_phone)
+                    # videos_str += "{}\t\t {}\t\t {}".format(title, genre, length)
+                    # new_video = "{}, {}, {}\n".format(title, genre, length)
+                    # self.__videos.append(new_video)
+        
+        return destinations_str
+        # return self.__videos
+>>>>>>> Stashed changes
